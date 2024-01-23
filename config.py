fc3d0b8b7c2d5040bebd888a6e4ccb14127aba99
JeseKi <2094901072@qq.com>
2024-01-23 22:21:05 +0800

    修复了无法导入配置文件的bug

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import json
 from config import Config , load_config , conf
 
-with open('plugins/zhipuai_plugin/config.json', 'r', encoding='utf-8') as f:
+with open('plugins/chatgpt-on-wechat-zhipuai-plugin/config.json', 'r', encoding='utf-8') as f:
     config = json.load(f)
 print(config)
